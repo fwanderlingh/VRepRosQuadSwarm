187a35e7d8c73a5a932d4e0886d27261b4700e55
merosss <fwanderlingh@gmail.com>
2014-05-21 18:43:49 +0200

    commit fix

---
+++ include/quadcopter_ctrl/Cao.h
@@ -0,0 +1,26 @@
+/*
+ * Cao.h
+ *
+ *  Created on: Apr 4, 2014
+ *      Author: francescow
+ */
+
+#ifndef CAO_H_
+#define CAO_H_
+
+#include <iostream>
+
+class Cao
+{
+private:
+  int state;
+
+public:
+  Cao();
+  virtual ~Cao();
+
+  static void SayHello();
+
+};
+
+#endif /* CAO_H_ */
